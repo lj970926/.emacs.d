7da21ef77fcb9205ee528bc64854f521837ef0d5
lj970926 <1783973490@qq.com>
2021-02-25 13:12:34 +0800

    commit elpa

--- init.el
+++ init.el
@@ -7,16 +7,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(org dracula-theme evil)))
+ '(auto-save-default nil)
+ '(package-selected-packages '(magit ivy org-bullets org dracula-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#2e2e2e" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "ADBO" :family "FiraCode Nerd Font Mono")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "black" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight bold :height 143 :width normal :foundry "CTDB" :family "FiraCode Nerd Font Mono")))))
 (require 'evil)
 (evil-mode 1)
 (tool-bar-mode 0)
+(scroll-bar-mode 0)
 (load-theme 'dracula t)
 (menu-bar-mode 0)
 (setq x-select-enable-clipboard t)
@@ -30,3 +32,11 @@
 	    (flyspell-mode 1)))
 
 (global-linum-mode 1)
+(require 'org-bullets)
+(add-hook 'org-mode-hook
+	  (lambda ()
+	    (org-bullets-mode 1)))
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
